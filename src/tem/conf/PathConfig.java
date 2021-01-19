590fe2ea66b408f21fdf7301692da63b839e6e4e
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2021-01-19 16:12:21 +0100

    Use paths consistent with Dropbox provided data

--- src/tem/conf/PathConfig.java
+++ src/tem/conf/PathConfig.java
@@ -4,9 +4,9 @@ public class PathConfig {
 
 	public static String scriptDataPath = "data/scriptData/ThreeM09/";
 	
-	public static String originalDataPath = "data/originalData/ThreeM09/";
+	public static String originalDataPath = "data/Training data ThreeM09/";
 	
-	public static String testDataPath = "data/originalData/TestData/";
+	public static String testDataPath = "data/TestDataQ1000/TestDataQ1000";
 	
 	public static String modelParamsPath = "data/modelParams/";
 	
